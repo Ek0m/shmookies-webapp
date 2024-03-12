b44fd5c05547a8beffb5eaa12faf9dac75f2b546
Ek0m <91160565+Ek0m@users.noreply.github.com>
2024-03-12 11:59:45 +0100

    changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,31 +1,31 @@
 import NextAuth from "next-auth/next";
 
-
-
 export const authOptions = {
-    providers: [
+  providers: [
     {
-        id: "descope",
-        name: "Descope",
-        type: "oauth",
-        wellKnown: `https://api.descope.com/`+process.env.DESCOPE_API+`/.well-known/openid-configuration`,
-        authorization: { params: { scope: "openid email profile" } },
-        idToken: true,
-        clientId: process.env.DESCOPE_API,
-        clientSecret: process.env.DESCOPE_SECRET,
-        checks: ["pkce", "state"],
-        profile(profile) {
-            return {
-                id: profile.sub,
-                name: profile.name,
-                email: profile.email,
-                image: profile.picture,
-            }
-        },
-    }],
-    
-}  
+      id: "descope",
+      name: "Descope",
+      type: "oauth",
+      wellKnown:
+        `https://api.descope.com/` +
+        process.env.NEXTAUTH_DESCOPE_API +
+        `/.well-known/openid-configuration`,
+      authorization: { params: { scope: "openid email profile" } },
+      idToken: true,
+      clientId: process.env.NEXTAUTH_DESCOPE_API,
+      clientSecret: process.env.NEXTAUTH_SECRET,
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
+};
 
-
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
